86a73081dd33d518e55e10a0393d92bb165c3542
Andrey Ioch <dird15@mail.ru>
2019-01-09 14:04:16 +0700

    Added entity & dto intefaces

---
+++ MedPoint/Data/Entities/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPoint.Data.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+
+    [Serializable]
+    public class BaseEntity : IEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
---
+++ MedPoint/Data/Models/IDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace MedPoint.Data.Models
+{
+    public interface IDto
+    {
+        Guid Id { get; set; }
+    }
+
+    [Serializable]
+    public class BaseDto : IDto
+    {
+        public Guid Id { get; set; }
+    }
+}
