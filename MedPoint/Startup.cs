3885f2582b3c7dd4151198c3af9e3dac920290b8
Andrey Ioch <dird15@mail.ru>
2019-01-09 13:48:47 +0700

    Added configure autofac

--- MedPoint/Startup.cs
+++ MedPoint/Startup.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
+using Autofac;
+using Autofac.Configuration;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,11 +23,30 @@ namespace MedPoint
         }
 
         public IConfiguration Configuration { get; }
+        public IContainer Container { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            return ConfigureAutofac(services);
+        }
+
+        private IServiceProvider ConfigureAutofac(IServiceCollection services)
+        {
+            var module = new ConfigurationModule(Configuration);
+            var builder = new ContainerBuilder();
+            var dataAccess = Assembly.GetExecutingAssembly();
+
+            builder.RegisterModule(module);
+            builder.RegisterInstance(Configuration).As<IConfiguration>().SingleInstance();
+            builder.Populate(services);
+
+            builder.RegisterAssemblyTypes(dataAccess)
+                .Where(t => t.Namespace.StartsWith("MedPoint"))
+                .AsImplementedInterfaces();
+
+            return builder.Build().Resolve<IServiceProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
