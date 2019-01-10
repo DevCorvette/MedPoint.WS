587892c07e19a1020742caa41365bdf67c4191c3
Andrey Ioch <dird15@mail.ru>
2019-01-10 11:06:37 +0700

    Added MedPoint Exeptions

---
+++ MedPoint/Exceptions/MedPointExceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace MedPoint.Exceptions
+{
+    [Serializable]
+    public class MedPointException : Exception
+    {
+        public MedPointException() { }
+        public MedPointException(string message) : base(message) { }
+        public MedPointException(string message, Exception inner) : base(message, inner) { }
+        protected MedPointException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class EntityNotFoundException : MedPointException
+    {
+        public EntityNotFoundException() { }
+        public EntityNotFoundException(string message) : base(message) { }
+        public EntityNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected EntityNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+    
+}
