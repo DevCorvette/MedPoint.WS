26b4d77e67c5ef5278f39d6658377b05f120ef4c
Andrey Ioch <dird15@mail.ru>
2019-01-10 13:17:49 +0700

    Added TestObjectStore

---
+++ MedPoint/Stores/Impl/TestObjectStore.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MedPoint.Data;
+using MedPoint.Data.Entities;
+using MedPoint.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPoint.Stores.Impl
+{
+    public class TestObjectStore : BaseStore<TestObject, TestObjectDto>, ITestObjectStore
+    {
+        public TestObjectStore(IMapper mapper, MedPointDbContext dbContext) : base(mapper, dbContext)
+        {
+        }
+    }
+}
