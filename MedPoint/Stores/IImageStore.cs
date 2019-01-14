b3c77b3dc9d397b657c055e53da5718a678633ab
Andrey Ioch <dird15@mail.ru>
2019-01-15 00:07:26 +0700

    Added ImageStore

---
+++ MedPoint/Stores/IImageStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MedPoint.Stores
+{
+    public interface IImageStore
+    {
+        Task SaveImage(string imageName, byte[] image);
+        void DeleteImage(string imageName);
+    }
+}
---
+++ MedPoint/Stores/Impl/ImageStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace MedPoint.Stores.Impl
+{
+    public class ImageStore : IImageStore
+    {
+        private AppSettings AppSettings { get; }
+
+        public ImageStore(IOptions<AppSettings> options)
+        {
+            AppSettings = options.Value;
+        }
+
+        public async Task SaveImage(string imageName, byte[] image)
+        {
+            var imageDirectory = Path.Combine(AppSettings.ResourcePaths.Base, AppSettings.ResourcePaths.Images);
+            Directory.CreateDirectory(imageDirectory);
+            var imagePath = Path.Combine(imageDirectory, imageName);
+
+            await File.WriteAllBytesAsync(imagePath, image);
+        }
+
+        public void DeleteImage(string imageName)
+        {
+            var imagePath = Path.Combine(AppSettings.ResourcePaths.Base, AppSettings.ResourcePaths.Images, imageName);
+            File.Delete(imagePath);
+        }
+    }
+}
