d9d4fc15300f5e9aa7c6479a94ea8f3700507bbd
Andrey Ioch <dird15@mail.ru>
2019-01-15 00:06:37 +0700

    Added image uris to app settings

---
+++ MedPoint/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPoint
+{
+    public class AppSettings
+    {
+        public ResourcePaths ResourcePaths { get; set; }
+        public Urls Urls { get; set; }
+    }
+
+    public class ResourcePaths
+    {
+        public string Base { get; set; }
+        public string Images { get; set; }
+    }
+
+    public class Urls
+    {
+        public string Images { get; set; }
+    }
+}
